1f328ab6e751d4c2f2ae35f16927ba006aec4427
hyejeong3283 <hyejeong3283@gmail.com>
2023-03-22 11:13:57 +0900

    2023.03.22

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pw = "root";
+    $db = "hyejeong3283";
+    $connect = new mysqli($host, $user, $pw, $db);
+    $connect -> set_charset("utf-8");    
+
+    if(mysqli_connect_errno()){
+        echo "Database Connect False";
+    } else {
+        echo "Database Connect True";
+    }
+?>
