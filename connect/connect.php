a923df82631587867113dd768aac3905bb754a37
hyejeong3283 <hyejeong3283@gmail.com>
2023-03-27 17:35:32 +0900

    2023.03.27

--- connect/connect.php
+++ connect/connect.php
@@ -2,13 +2,13 @@
     $host = "localhost";
     $user = "root";
     $pw = "root";
-    $db = "hyejeong3283";
+    $db = "sample";
     $connect = new mysqli($host, $user, $pw, $db);
     $connect -> set_charset("utf-8");    
 
     if(mysqli_connect_errno()){
         echo "Database Connect False";
     } else {
-        echo "Database Connect True";
+        // echo "Database Connect True";
     }
 ?>
